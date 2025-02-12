f987bf28b21bf8f685b1ebe1fb304ed2bffb77ce
InstaPay1 <instapay662@gmail.com>
2025-02-12 10:11:27 +0530

    Update client.js

--- client.js
+++ client.js
@@ -30,7 +30,7 @@ function initiatePayment() {
 	$.ajax({
            dataType: 'json',
             type: "POST",
-            url: 'https://pay.jigspay.com/newpayment.php',
+            url: 'BaseURL',
 			headers: {
     			"api_key": <<api_key>>
 				"signature" : signature
